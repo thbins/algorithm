2d009bebc37bd116ed17849715e9edafec6011f8
thbins <joons0218@gmail.com>
2025-12-10 19:30:48 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,26 +1,19 @@
-import math
-
 def solution(progresses, speeds):
-    answer = []
-    l1 = []
-    
-    # 남은 작업 일수 계산
-    for i in range(0, len(progresses)):
-        v1 = 100 - progresses[i]
-        v2 = v1 / speeds[i]
-        l1.append(math.ceil(v2))
-    
-    pre_value = l1[0]
-    count = 0
-    
-    for j in l1: 
-        if pre_value < j:
-            answer.append(count)
-            count = 1
-            pre_value = j
-            
-        elif pre_value >= j:
+    days = []
+    for p, s in zip(progresses, speeds):
+        days.append((100 - p + s - 1) // s)
+
+    result = []
+    leader = days[0]
+    count = 1
+
+    for d in days[1:]:
+        if d <= leader:
             count += 1
-            
-    answer.append(count)
-    return answer
+        else:
+            result.append(count)
+            leader = d
+            count = 1
+
+    result.append(count)
+    return result
